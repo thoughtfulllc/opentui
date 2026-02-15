7f2ea16651dd0559dea6a4942f529e05829dae03
Sebastian Herrlinger <hasta84@gmail.com>
2026-02-15 23:45:14 +0100

    plugin setup with renderer ref

--- packages/core/src/plugins/types.ts
+++ packages/core/src/plugins/types.ts
@@ -1,3 +1,5 @@
+import type { CliRenderer } from "../renderer"
+
 export interface PluginContext {}
 
 export type SlotRenderer<TNode, TProps, TContext extends PluginContext = PluginContext> = (
@@ -8,7 +10,7 @@ export type SlotRenderer<TNode, TProps, TContext extends PluginContext = PluginC
 export interface Plugin<TNode, TSlots extends object, TContext extends PluginContext = PluginContext> {
   id: string
   order?: number
-  setup?: (ctx: Readonly<TContext>) => void
+  setup?: (ctx: Readonly<TContext>, renderer: CliRenderer) => void
   dispose?: () => void
   slots: {
     [K in keyof TSlots]?: SlotRenderer<TNode, TSlots[K], TContext>
