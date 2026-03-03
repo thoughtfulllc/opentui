ad98e3a0be5fc31bf585e150e7d65559993f2cdb
Sebastian Herrlinger <hasta84@gmail.com>
2026-03-04 00:38:40 +0100

    resolve solid paths for external plugins

--- packages/solid/scripts/solid-plugin.ts
+++ packages/solid/scripts/solid-plugin.ts
@@ -3,12 +3,38 @@ import { readFile } from "node:fs/promises"
 // @ts-expect-error - Types not important.
 import ts from "@babel/preset-typescript"
 // @ts-expect-error - Types not important.
+import moduleResolver from "babel-plugin-module-resolver"
+// @ts-expect-error - Types not important.
 import solid from "babel-preset-solid"
 import { type BunPlugin } from "bun"
 
 const solidTransformPlugin: BunPlugin = {
   name: "bun-plugin-solid",
   setup: (build) => {
+    const moduleName = import.meta.resolve("@opentui/solid")
+    const canonical = [/^@opentui\/solid(?:\/.*)?$/, /^@opentui\/core(?:\/.*)?$/, /^solid-js(?:\/.*)?$/]
+
+    for (const filter of canonical) {
+      build.onResolve({ filter }, (args) => {
+        return {
+          path: import.meta.resolve(args.path),
+        }
+      })
+    }
+
+    const resolve = (path: string) => {
+      if (path.startsWith("@opentui/solid")) {
+        return import.meta.resolve(path)
+      }
+      if (path.startsWith("@opentui/core")) {
+        return import.meta.resolve(path)
+      }
+      if (path === "solid-js" || path.startsWith("solid-js/")) {
+        return import.meta.resolve(path)
+      }
+      return null
+    }
+
     build.onLoad({ filter: /\/node_modules\/solid-js\/dist\/server\.js$/ }, async (args) => {
       const path = args.path.replace("server.js", "solid.js")
       const code = await readFile(path, "utf8")
@@ -29,11 +55,21 @@ const solidTransformPlugin: BunPlugin = {
         //   },
         // },
         // plugins: [["solid-refresh/babel", { bundler: "esm" }]],
+        plugins: [
+          [
+            moduleResolver,
+            {
+              resolvePath(path: string) {
+                return resolve(path)
+              },
+            },
+          ],
+        ],
         presets: [
           [
             solid,
             {
-              moduleName: "@opentui/solid",
+              moduleName,
               generate: "universal",
             },
           ],
